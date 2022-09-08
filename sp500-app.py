1fb6582064a9bef63d0a18b22a432e07c6358dfa
Jordan Harris <jordan.lee.harris@icloud.com>
2022-09-08 21:39:09 +0100

    Update sp500-app.py

--- sp500-app.py
+++ sp500-app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
 
-st.title('S&P 500 App')
+st.title('Jordans S&P500 App')
 
 st.markdown("""
 This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
